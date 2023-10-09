acea49966c4cc2f9e41b8f79cfed626235a405cb
Gustavo Santos <gupaulasan@gmail.com>
2023-10-09 16:57:00 -0300

    Update densificationModel.py

--- densificationModel.py
+++ densificationModel.py
@@ -2,10 +2,7 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-import sys
-import time
-import math
-
+#----------------------------------
 def read_clean_csv(file, double_headed = True, x='temperatureC', y='density'):
     '''
     Reads and cleans a csv imported from WebPlotDigitilizer
@@ -30,9 +27,11 @@ def read_clean_csv(file, double_headed = True, x='temperatureC', y='density'):
     else:
         df = pd.read_csv(file,header=None)
         df.columns = [x,y]
+    df = df.sort_values(by=y, axis=0)
+    df.reset_index(inplace=True, drop=True)
     return df
 
-def gomez_hotza(Tmax:float, Tinit:float, beta:float, n:float, A: float, Ea: float, theta0: float, G:float, total_time:float, dt:float, R = 8.31446262, beta_SI = False, xaxis = 'Time', yaxis='Density'):
+def gomez_hotza(Tmax:float, Tinit:float, beta:float, n:float, A: float, Ea: float, theta0: float, G:float, total_time:float, dt:float, R = 8.31446262, beta_SI = False, as_df=False):
     '''
     Returns a 2D array with the values of time or temperature and the density or porosity of a material in a sintering process
     
@@ -51,14 +50,6 @@ def gomez_hotza(Tmax:float, Tinit:float, beta:float, n:float, A: float, Ea: floa
     xaxis: weather the X axis is 'Temperature' or 'Time'
     yaxis: weather the Y axis is 'Density' or 'Porosity'
     '''
-
-    validX = ['Temperature', 'Time']
-    validY = ['Density', 'Porosity']
-
-    if xaxis not in validX:
-        raise ValueError('xaxis: must be one of %r.' % validX)
-    if yaxis not in validY:
-        raise ValueError('yaxis: must be one of %r.' % validY)
 
     #Fixed numbers    
     tsteps = int(total_time/dt)
@@ -110,10 +101,15 @@ def gomez_hotza(Tmax:float, Tinit:float, beta:float, n:float, A: float, Ea: floa
     results[1,:] = T
     results[2,:] = rho
     results[3,:] = theta
+
+    if as_df:
+        results = pd.DataFrame(results.T, columns=['time','temperatureK', 'density', 'porosity'])
+        results['time_min'] = results['time']/60
+        results['temperatureC'] = results['temperatureK'] - 273
         
     return results
 
-def validation(df, n_values:list, A_values:list, Ea_values:list,  Tmax:float, Tinit:float, beta:float, theta0: float, G:float, R = 8.31446262, beta_SI = False, xaxis = 'Time', yaxis='Density'):
+def validation(df, dfX:object, dfY:object, n_values:list, A_values:list, Ea_values:list,  Tmax:float, Tinit:float, beta:float, theta0: float, G:float, R = 8.31446262, beta_SI = False):
     '''
     Applies a 3D grid search to the gomez_hotza() function.
     
@@ -131,8 +127,13 @@ def validation(df, n_values:list, A_values:list, Ea_values:list,  Tmax:float, Ti
     xaxis: weather the X axis is 'Temperature' or 'Time'
     yaxis: weather the Y axis is 'Density' or 'Porosity'
     '''
-    if ('temperatureC' or 'density') not in df.columns:
-        raise ValueError('df must have a "temperatureC" and a "density" column.')
+    validX = ['temperatureK', 'time', 'temperatureC', 'time_min']
+    validY = ['density', 'porosity']
+
+    if dfX not in validX:
+        raise ValueError('xaxis: must be one of %r.' % validX)
+    if dfY not in validY:
+        raise ValueError('yaxis: must be one of %r.' % validY)
     
     if not beta_SI:
         betaSI = beta/60
@@ -141,30 +142,64 @@ def validation(df, n_values:list, A_values:list, Ea_values:list,  Tmax:float, Ti
     
     total_time = (Tmax - Tinit)/betaSI
     dt = total_time/len(df)
+    best_error = float('inf')
+    list_of_dictionary = []
     
     for A in A_values:
         for Ea in Ea_values:
             for n in n_values:
-                result = gomez_hotza(Tmax=Tmax, Tinit=Tinit,beta=beta, theta0=theta0, G=G, A=A, Ea=Ea, n=n, total_time=total_time, dt=dt, R=R, beta_SI=beta_SI, xaxis=xaxis, yaxis=yaxis)
+                error_df = pd.DataFrame()
+                results = gomez_hotza(Tmax=Tmax, Tinit=Tinit,beta=beta, theta0=theta0, G=G, A=A, Ea=Ea, n=n, total_time=total_time, dt=dt, R=R, beta_SI=beta_SI, as_df=True)
+                error_df[dfX] = df[dfX]
+                error_df['pred_results'] = results[dfY]
+                error_df['ref_results'] = df[dfY].sort_values()
+                error_df['squared_error'] = (error_df['pred_results'] - error_df['ref_results']) **2
+                error = np.mean(error_df['squared_error'])
+                rmse = np.sqrt(error)
+                list_of_dictionary.append({'A':A, 'Ea':Ea,'n':n, 'RMSE':rmse})
+                if rmse < best_error:
+                    best_error = rmse
+                    best_params = {'A':A, 'Ea':Ea, 'n':n, 'RMSE':rmse}
+                    best = error_df
+    data = pd.DataFrame(list_of_dictionary, columns=['A', 'Ea', 'n', 'RMSE'])
+    print(f'Best params: {best_params}')
+    return best, data
+#---------------------------------------------------------------------------
 
-alumina = gomez_hotza(Tmax=1150,
-                      Tinit=25,
-                      beta=5,
-                      n=0.4,
-                      A=2000,
-                      Ea=210e3,
-                      theta0=0.45,
-                      G=0.4e-6,
-                      total_time=2e4,
-                      dt=5
-                      )
+duran = read_clean_csv("C:/Users/gupau/Documents/UFSC - PC/Semestre 23.2/TCC/Duran/duran.csv", double_headed=False)
 
-alumina_df = pd.DataFrame(alumina.T, columns=['time','temperatureK', 'density', 'porosity'])
-alumina_df['time_min'] = alumina_df['time']/60
-alumina_df['temperatureC'] = alumina_df['temperatureK'] - 273
-print(alumina_df.head())
+teste, params = validation(duran,
+                            dfX='temperatureC',
+                            dfY='density',
+                            n_values=[0.35,0.4,0.45],
+                            A_values=[400,550,600],
+                            Ea_values=[110e3,210e3,310e3],
+                            Tmax=1386.22,
+                            Tinit=406.415,
+                            beta=5,
+                            theta0=0.596372,
+                            G=10e-9
+                            )
+print(teste)
+print(params)
 
-sns.lineplot(data = alumina_df, x='time_min', y='density')
-plt.xlabel('Time, min')
-plt.ylabel('Density')
+ytzp = gomez_hotza(Tmax=1386.22,
+                   Tinit=406.41,
+                   beta=5,
+                   n=0.435,
+                   A=544.5,
+                   Ea=206e3,
+                   theta0=0.6,
+                   G=10e-9,
+                   total_time=2e4,
+                   dt=5,
+                   as_df=True
+                   )
+
+sns.lineplot(data=teste, x='temperatureC', y='pred_results', label='Prediction', marker='o',)
+sns.lineplot(data=teste, x='temperatureC', y='ref_results', label='Reference', marker='X')
+sns.lineplot(data=ytzp, x='temperatureC', y='density', label='teste')
+plt.ylabel('Fractional Density')
+plt.xlabel('Temperature, C')
+plt.legend()
 plt.show()
